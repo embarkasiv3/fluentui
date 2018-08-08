embarkasiv3/fluentui

commit dc0a9908162770fd94c1316be4938228abe26d72

Remove customizable decorator usage: part 1 (#5569)
* This change removes almost all uses of @customizable, as the logic was merged into the styled helper.

* Adding change files.

* React.createRef doesn't work. Took the time to make it work, and normalize all componentRef typings to use the normalized interface.

* Unit test updates.

* Minor fixes.

* Fixing change files.

* fixed snapshot.

* removing bad code.

* Adding back scss file.

packages/office-ui-fabric-react/src/components/Layer/Layer.tsx
@@ -3,4 +3,6 @@ import { ILayerProps, ILayerStyleProps, ILayerStyles } from './Layer.types';
 import { LayerBase } from './Layer.base';
 import { getStyles } from './Layer.styles';
 
-export const Layer = styled<ILayerProps, ILayerStyleProps, ILayerStyles>(LayerBase, getStyles);
+export const Layer = styled<ILayerProps, ILayerStyleProps, ILayerStyles>(LayerBase, getStyles, undefined, {
+  scope: 'Layer'
+});

commit 2e1e7f14f24a531c78304ba8aa631c0d2c7d2d71

Layer: Fix layer customizability regression introduced in #5569 - which also broke the official Layer example. (#5843)
* Fix customizable regression in Layer

* Change file

packages/office-ui-fabric-react/src/components/Layer/Layer.tsx
@@ -4,5 +4,6 @@ import { LayerBase } from './Layer.base';
 import { getStyles } from './Layer.styles';
 
 export const Layer = styled<ILayerProps, ILayerStyleProps, ILayerStyles>(LayerBase, getStyles, undefined, {
-  scope: 'Layer'
+  scope: 'Layer',
+  fields: ['hostId', 'theme', 'styles']
 });

packages/office-ui-fabric-react/src/components/Layer/examples/Layer.Customized.Example.tsx
@@ -59,7 +59,8 @@ export class LayerCustomizedExample extends React.Component<{}, ILayerCustomized
           style={{
             position: 'relative',
             height: '400px',
-            overflow: 'hidden'
+            overflow: 'hidden',
+            border: '1px solid #ccc'
           }}
         />
       </div>

packages/office-ui-fabric-react/src/components/__snapshots__/Layer.Customized.Example.tsx.shot
@@ -341,6 +341,7 @@ exports[`Component Examples renders Layer.Customized.Example.tsx correctly 1`] =
     id="test"
     style={
       Object {
+        "border": "1px solid #ccc",
         "height": "400px",
         "overflow": "hidden",
         "position": "relative",